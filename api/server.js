00bca42c82ca2ee0d92a66999148934c4277c7e8
Lokeshwar-V <lokeshwar619@gmail.com>
2023-12-04 22:53:28 +0530

    added delete option

--- api/server.js
+++ api/server.js
@@ -1,29 +1,46 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const cors=require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const app=express();
-
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://Sai1:sai123@cluster0.qlqlmdp.mongodb.net/",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+mongoose.connect("mongodb+srv://Sai1:sai123@cluster0.qlqlmdp.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-.then(()=>console.log("connected to db"))
-.catch(console.error)
+    .then(() => console.log("connected to db"))
+    .catch(console.error)
 
-const Todo=require('./models/Todo');
+const Todo = require('./models/Todo');
 
-app.get('/todos',async(req,res)=>{
-    const todos=await Todo.find();
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
     res.json(todos);
 });
 
-app.get('/todo/new',(req,res)=>{
+app.post('/todo/new', async (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    })
 
+    await todo.save();
+
+    res.send(todo);
 });
 
-app.listen(3001,()=>console.log("Server started on port 3000"));
+
+app.delete("/posts/:id", async (req, res) => {
+    try {
+        await Todo.deleteOne({ _id: req.params.id })
+        res.status(204).send('Deleted')
+    } catch {
+        res.status(404)
+        res.send({ error: "Post doesn't exist!" })
+    }
+})
+
+
+app.listen(3001, () => console.log("Server started on port 3000"));
